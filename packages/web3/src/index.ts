1a9eee783786aed84bd92df5d31c8b186e915a21
Molnár Botond <45335158+Gepsonka@users.noreply.github.com>
2024-10-08 20:49:44 +0200

    found right tsconfig for creating tpescript modules

--- packages/web3/src/index.ts
+++ packages/web3/src/index.ts
@@ -1,5 +1,5 @@
-import tokenContractMetadata from "../artifacts/ERC1155Token_metadata.json";
-import tokenMarketplaceMetadata from "../artifacts/ERC1155Marketplace_metadata.json";
+import tokenContractMetadata from "../artifacts/ERC1155Token_metadata.json" assert { type: "json" };
+import tokenMarketplaceMetadata from "../artifacts/ERC1155Marketplace_metadata.json" assert { type: "json" };
 
 export const tokenContractAbi = tokenContractMetadata.output.abi;
 export const tokenMarketplaceAbi = tokenMarketplaceMetadata.output.abi;
