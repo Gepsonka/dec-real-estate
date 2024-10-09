be6b935608a8845d30b6cd3513e6713cdddf416f
Molnár Botond <45335158+Gepsonka@users.noreply.github.com>
2024-10-10 01:41:51 +0200

    replaced orbitdb with mongo

--- apps/blockchain-event-listener/src/index.ts
+++ apps/blockchain-event-listener/src/index.ts
@@ -3,7 +3,8 @@ dotenv.config();
 
 import { viemPublicClient } from "./utils/blockchain/viemClient.ts";
 import { tokenContractAbi, tokenMarketplaceAbi } from "@repo/web3";
-import { onLogs } from "./utils/blockchain/eventDispatcher.ts";
+import { onLogsTokenContract } from "./utils/blockchain/eventDispatcher.ts";
+import * as db from "./utils/db/index.ts";
 
 console.log("rpc url: ", process.env.CHAIN_RPC_URL);
 console.log(
@@ -16,15 +17,14 @@ console.log(
   process.env.MARKETPLACE_CONTRACT_ADDRESS as `0x${string}`
 );
 
-const tokenContractUnwatch = viemPublicClient.watchContractEvent({
+viemPublicClient.watchContractEvent({
   address: process.env.TOKEN_CONTRACT_ADDRESS as `0x${string}`,
   abi: tokenContractAbi,
-  eventName: "TokenCreated",
-  onLogs: onLogs,
+  onLogs: onLogsTokenContract,
 });
 
-// const marketplaceContractUnwatch = viemPublicClient.watchContractEvent({
-//   address: [process.env.MARKETPLACE_CONTRACT_ADDRESS as `0x${string}`],
-//   abi: tokenMarketplaceAbi,
-//   onLogs: (logs) => console.log(logs),
-// });
+viemPublicClient.watchContractEvent({
+  address: [process.env.MARKETPLACE_CONTRACT_ADDRESS as `0x${string}`],
+  abi: tokenMarketplaceAbi,
+  onLogs: (logs) => console.log(logs),
+});
--- apps/blockchain-event-listener/src/utils/blockchain/eventDispatcher.ts
+++ apps/blockchain-event-listener/src/utils/blockchain/eventDispatcher.ts
@@ -1,15 +1,35 @@
 import { tokenContractAbi } from "@repo/web3";
 import { type Log, decodeEventLog } from "viem";
+import { tokenDb } from "../db/index.ts";
 
-export function onLogs(logs: Log[]) {
-  console.log(logs);
-  logs.forEach((log) => {
-    console.log(
-      decodeEventLog({
-        abi: tokenContractAbi,
-        data: log.data,
-        topics: log.topics,
-      })
-    );
+export interface TokenCreatedEventArgs {
+  tokenId: BigInt;
+  creatorAddress: `0x${string}`;
+  amount: BigInt;
+  data: `0x${string}`;
+}
+
+export function onLogsTokenContract(logs: Log[]) {
+  const decodedEventLogs = logs.map((log) => {
+    return decodeEventLog({
+      abi: tokenContractAbi,
+      data: log.data,
+      topics: log.topics,
+    });
+  });
+
+  decodedEventLogs.forEach((decodedLog) => {
+    console.log(decodedLog);
+  });
+
+  decodedEventLogs.forEach(async (decodedLog) => {
+    const args: TokenCreatedEventArgs =
+      decodedLog.args as unknown as TokenCreatedEventArgs;
+
+    const result = await tokenDb.db.put({
+      ...args,
+    });
   });
 }
+
+export function onLogsMarketplaceContract(logs: Log[]) {}
